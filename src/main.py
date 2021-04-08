fac699574d22b2398d719b5c806c18c039445437
Anastasiia Terenteva <terenteva8412@gmail.com>
2021-04-08 18:30:30 +0200

    Added more test graphs.
    Signed-off-by: Anastasiia Terenteva <terenteva8412@gmail.com>

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from datavis.Graph import Graph
 from datavis.LayeredTreeDraw import LayeredTreeDraw
 
 g = Graph()
-g.load_graphml("test_trees/tree-84n.xml")
+g.load_graphml("test_files/bitrees/tree-828n.xml")
 
 ltd = LayeredTreeDraw(g)
 ltd.set_coordinates()
